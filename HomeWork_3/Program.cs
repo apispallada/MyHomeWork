4c817e08d4c9faa4fc4e9b5fbad813fa2868107c
Alena <apispallada@gmail.com>
2022-05-01 18:33:40 +0300

    HomeWork_3 changes

--- HomeWork_3/Program.cs
+++ HomeWork_3/Program.cs
@@ -2,24 +2,24 @@
 Задача 19: напишите программу, которая принимает на вход пятизначное число и проверяет, является ли 
 оно палиндромом.
 
-void PalindromNumber(int num)
+void PalindromNumber(double num)
 {         
     if (num <= 9999 || num >= 100000)
         Console.Write("Incorrect number!");
     else
     {
-        int n1 = num/10000;
-        int n2 = (num/1000)%10;
-        int n3 = (num/10)%10;
-        int n4 = num%10;
+        double n1 = num/10000;
+        double n2 = (num/1000)%10;
+        double n3 = (num/10)%10;
+        double n4 = num%10;
         if (n1==n4 && n2==n3)
             Console.Write("Yes!");
         else
              Console.Write("No!");
     }
 }
-Console.Write("Input integer five-digit number: ");
-int num = Convert.ToInt32(Console.ReadLine());  
+Console.Write("Input doubleeger five-digit number: ");
+double num = Convert.Todouble32(Console.ReadLine());  
 PalindromNumber(num);
 */
 
@@ -28,7 +28,7 @@ PalindromNumber(num);
 Задача 21: напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними 
 в 3D пространстве.
 
-double DistanceDtD(int xA, int yA, int zA, int xB, int yB, int zB)
+double DistanceDtD(double xA, double yA, double zA, double xB, double yB, double zB)
 {
     return Math.Sqrt((xB-xA)*(xB-xA)+(yB-yA)*(yB-yA)+(zB-zA)*(zB-zA));
 }
@@ -44,19 +44,18 @@ Console.WriteLine("Введите координату y второй точки
 int yB = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Введите координату z второй точки: ");
 int zB = Convert.ToInt32(Console.ReadLine());
-//Console.WriteLine(Sqrt1(xA, yA, zA, xB, yB, zB));
- Console.Write("A("  + xA + "," + yA + "," + zA + "); B(" + xB + "," + yB + "," + zB + ") -> " + DistanceDtD(xA, yA, zA, xB, yB, zB));
+Console.Write("A("  + xA + "," + yA + "," + zA + "); B(" + xB + "," + yB + "," + zB + ") -> " + DistanceDtD(xA, yA, zA, xB, yB, zB));
 */
 
 
 /*
 Задача 23: напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
 
-void InCube(int N)
+void InCube(double N)
 {
     if (N > 0)
     {
-        int count = 1;
+        double count = 1;
         Console.Write( N + " -> ");
         while (count <= N)
         {
@@ -66,7 +65,7 @@ void InCube(int N)
     }
     else
     {
-        int count = -1;
+        double count = -1;
         Console.Write( N + " -> ");
         while (count >= N)
         {
@@ -77,7 +76,7 @@ void InCube(int N)
         
 }
 Console.WriteLine("Введите целое число: ");
-int num = Convert.ToInt32(Console.ReadLine());
+double num = Convert.Todouble32(Console.ReadLine());
 InCube(num);
 */
         
