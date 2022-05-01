273c4a08bd9b70a1ef2b481e640a87632a263181
Alena <apispallada@gmail.com>
2022-05-02 01:06:30 +0300

    HomeWork_2 mistake deleted

--- HomeWork_2/Program.cs
+++ HomeWork_2/Program.cs
@@ -25,73 +25,10 @@ void ShowSecondNumber(int num)
 Console.Write("Input integer three-digit number: ");
 int num = Convert.ToInt32(Console.ReadLine());
 ShowSecondNumber(num);
-*/
-/*
+
+
 Задача 13: Написать программу, которая выводит третью цифру заданного числа
 или сообщает, что третьей цифры нет.
-*/
-/*
-int ThirdDigit(int num)
-{
-    if (num > -100 & num < 100)
-        Console.Write("Third digit isn't exist!");
-    
-    
-    else
-    {
-        if (num > 999 & num < -999)
-        {
-            while (num > 999 || num < - 999)
-                num = num/10;
-
-        }
-        
-        
-        
-        if (num > 99 & num < 1000)
-        {
-            int n1 = num%10;
-            Console.Write(num + " -> Third digit is " + n1);
-        }
-        if (num > -1000 & num < -99)
-        {
-            int num1 = num%10;
-            int num2 = -1*num1;
-            Console.Write(num  + " -> Therd digit is " + num2);
-        }
-          
-    }
-
-}
-Console.Write("Input integer number: ");
-int num = Convert.ToInt32(Console.ReadLine());
-ThirdDigit(num);
-*/
-/*
-Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день 
-недели, и проверяет, является ли этот день выходным.
-
-void ShowWeekDay(int num)
-{
-    if (num < 1 || num > 7)
-        Console.Write("Incorrect number!");
-    else
-    {
-        if (num == 6 || num == 7)
-            Console.Write("Wow! it's a day off!");
-        else
-            Console.Write("Sorry, it's a working day. ");
-        
-    }
-}
-Console.Write("Input numder of weekday: ");
-int day = Convert.ToInt32(Console.ReadLine());
-ShowWeekDay(day);
-*/
-
-
-
-
 
 void ThirdDigit(int num)
 {
@@ -131,3 +68,30 @@ void ThirdDigit(int num)
 Console.Write("Input integer number: ");
 int num = Convert.ToInt32(Console.ReadLine());
 ThirdDigit(num);
+
+
+Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день 
+недели, и проверяет, является ли этот день выходным.
+
+void ShowWeekDay(int num)
+{
+    if (num < 1 || num > 7)
+        Console.Write("Incorrect number!");
+    else
+    {
+        if (num == 6 || num == 7)
+            Console.Write("Wow! it's a day off!");
+        else
+            Console.Write("Sorry, it's a working day. ");
+        
+    }
+}
+Console.Write("Input numder of weekday: ");
+int day = Convert.ToInt32(Console.ReadLine());
+ShowWeekDay(day);
+*/
+
+
+
+
+
